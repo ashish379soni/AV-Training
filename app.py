7a300dd1e2f6f1415a02896b2f03eda4dd51fa64
Ashish Swarnkar <ashishswarnkar@Ashishs-MacBook-Air.local>
2025-06-21 16:27:01 +0530

    learning docker

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from area import circle
+
+st.title("Welcome to My Streamlit App!")
+st.write("This is a simple beginner-level Streamlit application.")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.write(f"Hello, {name}! 👋")
+
+number = st.slider("Pick a number", 1, 100, 50)
+st.write(f"You selected: {number}")
+
+radius = st.number_input("Enter the radius of circle:")
+if radius:
+    st.write(circle(r=radius))
+
+if st.button("Say Hello"):
+    st.success("Hello Streamlit User!")
---
+++ area.py
@@ -0,0 +1,3 @@
+from math import pi
+def circle(r:float)->float:
+    return pi*r*r
