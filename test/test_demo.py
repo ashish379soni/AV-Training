2e0517ec92d3dd926a1879dd51f4a93cfd9d0b6f
Ashish Kumar Swarnkar <ashish379soni@gmail.com>
2025-06-22 14:49:14 +0530

    pytest changes

--- test/test_demo.py
+++ test/test_demo.py
@@ -1,7 +1,12 @@
+"""Test using pytest"""
 import pytest
 from area import circle
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 def test_area_of_circle():
+    """Basic tests"""
     assert circle(5) == pytest.approx(78.54, rel=1e-2)
     assert circle(0) == 0
     assert circle(1) == pytest.approx(3.14, rel=1e-2)
